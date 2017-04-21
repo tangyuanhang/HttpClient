7a8e69507f932117495ba4ee2ee8e1fb5ac6bc7d
tang <18711624029@163.com>
2017-04-21 18:14:39 +0800

    哈哈

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,17 +1,10 @@
-import Utils.Config;
-import org.apache.log4j.Logger;
+
 
 /**
  * Created by ${user} on 2017-04-13.
  */
 public class Demo {
-    private static Logger logger = Logger.getLogger(Demo.class);
     public static void main(String[] args) {
-        //  记录info 级别的信息
-            logger.info("This is info message.");
-        //  记录error 级别的信息
-            logger.error("This is error message.");
-            System.out.println(Config.HOST_URL);
-            System.out.println(Config.REQUEST_TYPE);
+
         }
 }
